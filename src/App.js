d02af59acfd380c411f4835702aadf0be9ddb44c
mehedi hasan <mehedicsebu@gmail.com>
2022-04-26 16:23:49 +0600

    bootstrap css added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home/Home';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h2>
-        Home Page
-      </h2>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
